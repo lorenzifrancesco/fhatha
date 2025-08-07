81c246d9a190ae0149a806fa1c0f69419c260c52
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2025-08-08 00:18:26 +0200

    add: methods to reproduce QFHT and FHATHA, some testing for it

--- fhatha/transform.py
+++ fhatha/transform.py
@@ -1,11 +1,64 @@
 import numpy as np
 from scipy.fft import fft, ifft
+from scipy.special import jv, j0
 
 
-def precompute_kernel():
-    return
 
-def fht(data: np.ndarray) -> np.ndarray:
+def correlate(fs, js):
+    return fft(fft(fs) * ifft(js))
+
+
+# SIEGMAN original method
+def naive_siegman(fs: np.ndarray, alpha: float, x0: float, y0: float) -> np.ndarray:
+    n_samples = len(fs)
+    fsp = np.pad(fs, (0, n_samples), mode='constant', constant_values=0)
+    xs = np.exp(np.arange(n_samples) * alpha) * x0
+    ys = np.exp(np.arange(n_samples) * alpha) * y0
+    js = precalculate_js(x0*y0, n_samples, alpha)
+    return xs, ys, correlate(fsp, js)
+
+# alpha = dln
+def precalculate_js(xy0, n_samples, alpha) -> np.ndarray:
+    xy_padded = xy0 * np.exp(np.arange((2 * n_samples)) * alpha)
+    return 2* np.pi * xy_padded * alpha * j0(2*np.pi * xy_padded)
+
+
+
+
+# MAGNI original method
+def magni_grid(n_samples: int, alpha: float, x0: float) -> tuple:
+    idx = np.arange((n_samples))
+    log_xy   = x0 * np.exp(idx * alpha)
+    log_xy[0] = 0.0
+    return log_xy
+
+def fhta_single(
+    fs: np.ndarray, 
+    alpha: float, 
+    x0: float, 
+    fresnel:float) -> np.ndarray:
     
-    result = fft(ifft() * fft())
-    return result 
+    n_samples = len(fs)
+    fsp = np.pad(fs, (0, n_samples), mode='constant', constant_values=0)
+    xs = magni_grid(n_samples, alpha, x0)
+    ys = xs
+    ysp = magni_grid(2*n_samples, alpha, x0)
+    k0 = 3/(8*alpha) + 1/2 # FIXME, still the approximation
+    
+    phis = (fsp - np.roll(fsp, -1)) * np.exp((np.arange(2*n_samples) - n_samples + 1) * alpha)
+    phis[0] *= k0
+    phis[n_samples:] *= 0.0
+    
+    js = precalculate_js_magni(x0, n_samples, alpha, fresnel)
+    print("lenght of js", len(js))
+    # remember x0 = y0 in Magni
+    return xs, ys, correlate(phis, js) / ysp / fresnel
+    
+def precalculate_js_magni(
+    x0, 
+    n_samples, 
+    alpha, 
+    fresnel) -> np.ndarray:
+    x_padded = x0 * np.exp((np.arange((2 * n_samples)) + 1 - n_samples)* alpha)
+    print("length of x_padded", len(x_padded))
+    return jv(1, 2 * np.pi * fresnel * x_padded)
--- tests/test_base.py
+++ tests/test_base.py
@@ -33,20 +33,32 @@ def test_scipy_compatibility():
     assert np.allclose(tr.fht(data), fht(data))
     assert np.allclose(tr.ifht(fht(data)), data)
     
-def test_siegman(function=flat_top, label="ft"):
+def test_siegman(function=flat_top, label="ft", fresnel=1):
     n_samples = 256
-    idx =  np.array(range(n_samples))
-    idxp = np.array(range(n_samples*2))
+    idx =  np.arange((n_samples))
+    idxp = np.arange((n_samples*2))
     # radius = np.linspace(0, 1, n_samples)
-    r0  = 0.063
+    r0  = 0.06349
     dln = 0.01612
+    
+    # r0  = 0.063
+    # dln = 0.01612
     log_r   = r0 * np.exp(idx * dln)
-    # log_k   = 1 / r0 * np.exp((idx - n_samples + 1) * dln)
-    # log_k_p = 1 / r0 * np.exp((idxp - n_samples + 1) * dln) # balanced
-    # change here the rescaling since then it gets inside the offset
-    log_k   =  r0 * np.exp((idx)  * dln) # siegman
-    log_k_p =  r0 * np.exp((idxp) * dln) # siegman
-    offset_lin = log_r[0] * log_k_p[-1]  * 2 * np.pi
+    
+    # fftlog
+    # log_k   = 1 / r0 * np.exp((idx - n_samples + 1) * dln) 
+    # log_k_p = 1 / r0 * np.exp((idxp - n_samples + 1) * dln)
+    
+    # magni
+    log_r   = 1 * np.exp((idx - n_samples + 1) * dln)
+    log_k   = 1 * np.exp((idx - n_samples + 1) * dln)
+    log_k_p = 1 * np.exp((idxp - (n_samples - 1)/2) * dln)
+    print(log_k)
+    # siegman
+    # log_k   =  r0 * np.exp((idx)  * dln)
+    # log_k_p =  r0 * np.exp((idxp) * dln)
+    
+    offset_lin = log_r[0] * log_k[-1] * 2 * np.pi * fresnel # accomodate the definition of Magni
     print(f"Offset linear: {offset_lin}")
     
     k0 = 1 / r0 * np.exp((-n_samples+1) * dln)
@@ -72,18 +84,21 @@ def test_siegman(function=flat_top, label="ft"):
     plt.savefig('media/'+label+'initial.png')
     plt.clf()
 
+    # physics
     exact = function(log_k_p, transform=True)
     plt.figure(figsize=(4, 2.5))
-    plt.plot(log_k_p * 2 * np.pi, (g/log_k_p) / np.max(g/log_k_p) * 1.5, label='Transformed Data')
+    plt.plot(log_k_p, g / (log_k_p * fresnel), label='Transformed Data')
     plt.plot(log_k_p, exact, label='Transformed Data')
     plt.xlabel(r'$k$')
     plt.xlim(0, n_samples/4)
+    plt.ylim(-0.5, 1.6)
     plt.tight_layout()
     plt.savefig('media/'+label+'_physical_transform.png')
     
-    exact = function(log_k_p, transform=True) * log_k_p
+    # samples
+    exact = function(log_k_p, transform=True) * (log_k_p * fresnel)
     plt.figure(figsize=(4, 2.5))
-    plt.plot(idxp, g, label='Transformed Data')
+    plt.plot(idxp, g * 2, label='Transformed Data')
     plt.plot(idxp, exact, label='Transformed Data')
     plt.xlabel('idx')
     plt.axvline(n_samples, color='red', linestyle='--', label='n_samples')
@@ -92,7 +107,44 @@ def test_siegman(function=flat_top, label="ft"):
     plt.tight_layout()
     plt.savefig('media/'+label+'_transformed_samples.png')
     
+
+def test_naive_siegman():
+    n_samples = 256
+    idx = np.arange((n_samples))
+    idxp = np.arange((2*n_samples))
+    r0  = 0.06349
+    dln = 0.01612
+    
+    # desired_r_max = 1.0
+    # dln = np.log(desired_r_max / r0) / (n_samples - 1)
+    
+    print("dln:", dln)
+    log_r   = r0 * np.exp(idx * dln)
+    print("selected range:", log_r[:4], "...", log_r[-4:])
+    print("last value:", log_r[-1])
+    # assert np.isclose(log_r[-1], 3.938)
+    log_k   = 1 / r0 * np.exp((idx - n_samples + 1) * dln)
+    
+    f = flat_top(log_r, transform=False)
+    fs = log_r * f
+    # fs = np.pad(fs, (0, n_samples), 'constant', constant_values=0) -> we are already padding
+    _, _, g = tr.naive_siegman(fs, dln, r0, r0)
+    _, _, g_m = tr.fhta_single(fs, dln, r0, fresnel = 3.1415)
+    
+    # assert np.isclose(g, 0.0)
+    plt.figure(figsize=(4, 2.5))
+    plt.plot(idxp, np.real(g),   lw=0.6, ls="-")
+    plt.plot(idxp, np.imag(g),   lw=0.6, ls="-")
+    plt.plot(idxp, np.real(g_m), lw=1.2, ls=":")
+    plt.plot(idxp, np.imag(g_m), lw=1.2, ls=":")
+    plt.xlabel('idx')
+    plt.tight_layout()
+    plt.savefig('media/naive.pdf')
+    
+
 if __name__ == "__main__":
-    test_siegman(function=flat_top, label="ft")
-    test_siegman(function=magni,    label="mg")
+    test_naive_siegman()
+    exit()
+    test_siegman(function=flat_top, label="ft", fresnel = 1)
+    test_siegman(function=magni,    label="mg", fresnel = 3)
     
