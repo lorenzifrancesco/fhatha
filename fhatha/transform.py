e3a7f984c1920d9b8b6765a9e8b7c80458f0d21e
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2025-08-15 09:52:28 +0200

    fix: strangeness of the Magni result

--- fhatha/transform.py
+++ fhatha/transform.py
@@ -1,14 +1,23 @@
 import numpy as np
 from scipy.fft import fft, ifft
 from scipy.special import jv, j0
+from scipy import optimize
+from matplotlib import pyplot as plt
+import logging as lg
 
+# One-time setup (do this near program start)
+lg.basicConfig(
+    level=lg.WARN,
+    format="%(asctime)s [%(levelname)s] %(message)s"  # basic format
+)
 
 
 def correlate(fs, js):
     return fft(fft(fs) * ifft(js))
 
-
 # SIEGMAN original method
+
+
 def naive_siegman(fs: np.ndarray, alpha: float, x0: float, y0: float) -> np.ndarray:
     n_samples = len(fs)
     fsp = np.pad(fs, (0, n_samples), mode='constant', constant_values=0)
@@ -18,47 +27,129 @@ def naive_siegman(fs: np.ndarray, alpha: float, x0: float, y0: float) -> np.ndar
     return xs, ys, correlate(fsp, js)
 
 # alpha = dln
+
+
 def precalculate_js(xy0, n_samples, alpha) -> np.ndarray:
     xy_padded = xy0 * np.exp(np.arange((2 * n_samples)) * alpha)
-    return 2* np.pi * xy_padded * alpha * j0(2*np.pi * xy_padded)
+    return 2 * np.pi * xy_padded * alpha * j0(2*np.pi * xy_padded)
 
 
 # MAGNI original method
-def magni_grid(n_samples: int, alpha: float, x0: float) -> tuple:
+def optimal_dln(n_samples: int) -> float:
+    def fun(alpha): return alpha + np.log(1 - np.exp(-alpha))/(n_samples - 1)
+    alpha = optimize.root_scalar(fun, bracket=[0, 10], method='bisect').root
+    # print("ALPHA:", alpha)
+    return alpha
+
+
+def magni_grid(n_samples: int) -> tuple:  # used for the sampling phase
+    alpha = optimal_dln(n_samples)
+    x0 = np.exp(-alpha*(n_samples))/2 * (1+np.exp(alpha))  # FIXME
+    # x0 = 0.01
+    # x0 *= 1.075 # FIXME
     idx = np.arange((n_samples))
-    log_xy   = x0 * np.exp(idx * alpha)
-    log_xy[0] = 0.0
-    return log_xy
+    log_xy = x0 * np.exp(idx * alpha)
+    # assert np.isclose(log_xy[0], x0) # this is true
+    # assert np.isclose(log_xy[-1], 1.0) #  this is not true
+    return log_xy, x0, alpha
+
+
+def magni_xi(n_samples: int, extend=False) -> np.array:
+    alpha = optimal_dln(n_samples)
+    if extend:
+        idx = np.arange(n_samples * 2)
+        xis = np.zeros(n_samples*2 + 1)
+    else:
+        idx = np.arange(n_samples)
+        xis = np.zeros(n_samples + 1)
+    xis = np.exp((idx-n_samples) * alpha)
+    xis[0] = 0.0
+    return xis
+
+
+def precalculate_js_magni(
+        x0,
+        n_samples,
+        alpha,
+        fresnel) -> np.ndarray:
+    # * (1-1/n_samples)
+    x_padded = x0 * \
+        np.exp((np.arange((2 * n_samples)) - n_samples + 1) * alpha)
+    # x_padded = (1+1/512) * x0 * np.exp((1+1/512) * (np.arange((2 * n_samples)) - n_samples)* alpha)
+    return jv(1, 2 * np.pi * fresnel * x_padded)
+
 
 def fhta_single(
-    fs: np.ndarray, 
-    alpha: float, 
-    x0: float, 
-    fresnel:float) -> np.ndarray:
-    
+        fs: np.ndarray,
+        fresnel: float) -> np.ndarray:
+
     n_samples = len(fs)
     fsp = np.pad(fs, (0, n_samples), mode='constant', constant_values=0)
-    xs = magni_grid(n_samples, alpha, x0)
+    # xs, x0, alpha = magni_grid(n_samples)
+
+    alpha = optimal_dln(n_samples)
+    x0 = np.exp(-alpha*(n_samples))/2 * (1+np.exp(alpha))  # FIXME
+    idx = np.arange((n_samples))
+    xs = x0 * np.exp(idx * alpha)
+    print("alpha: ", alpha)
+    xis = magni_xi(n_samples)
+    plt.scatter(range(n_samples), xis, s=0.1,
+                label="xi", marker='x', color='red')
+    plt.scatter(range(n_samples-1), xis[1:],
+                s=0.1, label="xi", marker='x', color="red")
+    plt.scatter(range(n_samples), xs, s=0.1, label="xi", marker='x')
+    plt.savefig("media/tests/xi_vs_x.pdf")
+    plt.close()
+    for i in range(n_samples-1):
+        lg.debug(
+            f"checking subinterval {i+1:>5}, : xi_n {xis[i]:.5f}, xi_n+1 {xis[i+1]:.5f} | avg= {(xis[i] + xis[i+1])/2:.5f} selected= {xs[i]:.5f}")
+        pass
     ys = xs
-    ysp = magni_grid(2*n_samples, alpha, x0)
-    k0 = 3/(8*alpha) + 1/2 # FIXME, still the approximation
+    ysp = np.pad(ys, (0, n_samples), mode='constant', constant_values=1)
+    k0 = 3/(8*alpha) + 1/2  # TODO, still the approximation
+    print("k0", k0)
+    fsp[n_samples] = 0.0
     fsp_rolled = np.roll(fsp, -1)
+    fsp_rolled[-1] = 0.0
     assert fsp_rolled[1] == fsp[2]
-    fsp_rolled[0] = 0.0
-    phis = (fsp - fsp_rolled) * np.exp((np.arange(2*n_samples) - n_samples + 1) * alpha)
+    phis = (fsp - fsp_rolled) * \
+        np.exp((np.arange(2*n_samples) - n_samples + 1) * alpha)
     phis[0] *= k0
     phis[n_samples:] *= 0.0
-    
+
     js = precalculate_js_magni(x0, n_samples, alpha, fresnel)
     print("lenght of js", len(js))
     # remember x0 = y0 in Magni
+    lg.warning(fsp)
     return xs, ys, correlate(phis, js) / ysp / fresnel
-    
-def precalculate_js_magni(
-    x0,
-    n_samples, 
-    alpha, 
-    fresnel) -> np.ndarray:
-    x_padded = x0 * np.exp((np.arange((2 * n_samples)) + 1 - n_samples)* alpha)
-    print("length of x_padded", len(x_padded))
-    return jv(1, 2 * np.pi * fresnel * x_padded)
+
+def foolproof_magni(
+        fun,
+        n_samples: int,
+        fresnel: float) -> np.ndarray:
+
+    # grid construction
+    idx = np.arange(n_samples)
+    alpha = optimal_dln(n_samples)
+    x0 = np.exp(-alpha*(n_samples))/2 * (1+np.exp(alpha))
+    print("x0", x0)
+    grid = x0 * np.exp(alpha * idx)
+    k0 = 3/(8*alpha) + 1/2  # TODO, still the approximation
+
+    fs = np.zeros(n_samples + 1)
+    fs[:n_samples] = fun(grid, transform=False)
+    fs[n_samples] = 0.0
+    phis = np.zeros(2*n_samples)
+    for i in range(2*n_samples):
+        if i < n_samples:
+            phis[i] = (fs[i]-fs[i+1]) * np.exp(alpha*(i+1-n_samples))
+        else:
+            phis[i] = 0
+    phis[0] *= k0
+
+    js = np.zeros(2*n_samples)
+    for i in range(2*n_samples):
+        js[i] = jv(1, 2*np.pi*fresnel*x0*np.exp((i+1-n_samples)*alpha))
+    convolution = fft(fft(phis) * ifft(js))
+    lg.warning(fs)
+    return 1, 1, convolution[:n_samples] / (fresnel * grid)
--- tests/test_base.py
+++ tests/test_base.py
@@ -15,8 +15,8 @@ def magni(x, transform=True):
 def flat_top(x, transform=True):
     if transform:
         # beware, we are using jv1, we should use jv0
-        b = 3.938
-        return b * jv(1, 2*np.pi* 3.938 * x) / (x) # only valid for the parameters alpha, beta, b, r0, rho0 in siegman
+        b = 1.0 # compatible with the specification of Magni
+        return b * jv(1, 2*np.pi* b * x) / (x) # only valid for the parameters alpha, beta, b, r0, rho0 in siegman
     else:
         return x/x
 
@@ -38,46 +38,73 @@ def test_siegman(function=flat_top, label="ft", fresnel=1):
     n_samples = 256
     idx =  np.arange((n_samples))
     idxp = np.arange((n_samples*2))
-    # radius = np.linspace(0, 1, n_samples)
-    r0  = 0.06349
-    dln = 0.01612
+
+    # # radius = np.linspace(0, 1, n_samples)
+    # r0  = 0.06349
+    # r_f = 1.0
+    # dln = np.log(r_f/r0) / (n_samples - 1)
     
-    # r0  = 0.063
-    # dln = 0.01612
-    log_r   = r0 * np.exp(idx * dln)
+    # # r0  = 0.063
+    # # dln = 0.01612
+    # log_r   = r0 * np.exp(idx * dln)
+    # assert np.isclose(log_r[-1], r_f)
     
-    # fftlog
-    # log_k   = 1 / r0 * np.exp((idx - n_samples + 1) * dln) 
-    # log_k_p = 1 / r0 * np.exp((idxp - n_samples + 1) * dln)
+    # # fftlog
+    # # log_k   = 1 / r0 * np.exp((idx - n_samples + 1) * dln) 
+    # # log_k_p = 1 / r0 * np.exp((idxp - n_samples + 1) * dln)
     
-    # magni
-    log_r   = 1 * np.exp((idx - n_samples + 1) * dln)
-    log_k   = 1 * np.exp((idx - n_samples + 1) * dln)
-    log_k_p = 1 * np.exp((idxp - (n_samples - 1)/2) * dln)
-    print(log_k)
+    # # magni
+    # log_r   = 1 * np.exp((idx - n_samples + 1) * dln)
+    # log_k   = 1 * np.exp((idx - n_samples + 1) * dln)
+    # log_k_p = 1 * np.exp((idxp - (n_samples - 1)/2) * dln)
+    # r0 = log_r[0]
+    # assert np.isclose(log_r[-1], r_f)
+    # assert np.isclose(log_k[-1], 1.0)
+    # print(log_k)
+
+    
+    # REDEFINITIONS according to the Magni scheme
+    r0 = 0.01
+    dln = np.log(1/r0) / (n_samples - 1)
+    # r0 = (1+np.exp(dln)) * np.exp(-dln / n_samples)/2
+    log_r = r0 * np.exp(idx * dln) # enforce the maximum to 1
+    log_r_p = r0 * np.exp((idxp - n_samples + 1) * dln) # this is the first value of the log_r, by construction
+    assert np.isclose(log_r[-1], 1.0)
+    log_k = log_r / r0
+    log_k_p = log_r_p / (r0)
+    k0 = log_k[0]
+    # print(log_r, log_k)
+    assert np.isclose(log_r[-1], 1/log_k[0])
+    assert np.isclose(log_r[0], 1/log_k[-1])
     # siegman
     # log_k   =  r0 * np.exp((idx)  * dln)
     # log_k_p =  r0 * np.exp((idxp) * dln)
     
-    offset_lin = log_r[0] * log_k[-1] * 2 * np.pi * fresnel # accomodate the definition of Magni
+    # make Magni sample in the correct space again
+   
+    offset_lin = log_r[0] * log_k[-1] * 2 * np.pi # * fresnel # accomodate the definition of Magni
     print(f"Offset linear: {offset_lin}")
     
-    k0 = 1 / r0 * np.exp((-n_samples+1) * dln)
-    r_end = r0 * np.exp((n_samples - 1) * dln)
-    assert(np.isclose(k0 * r_end, 1.0))
+    # k0 = 1 / r0 * np.exp((-n_samples+1) * dln)
+    # k0 = log_k[0] # this is the last value of the log_k
+    # r_end = r0 * np.exp((n_samples - 1) * dln)
+    # print(k0 * r_end)
     f = function(log_r, transform=False)
     
     # prepare the scipy f function from the Siegman (physical)
     fpy = log_r * f
     fpy = np.pad(fpy, (0, n_samples), 'constant', constant_values=0)
-    gpy = sp.fht(fpy, 
+    g_scipy = sp.fftshift(sp.fht(fpy, 
                dln=dln, 
                mu=0, 
                offset=np.log(offset_lin), 
                bias = 0.0)
+            )
     # get the Siegman transformed function from scipy
-    g = gpy # note that in the plots of Siegman we are plotting k g(k).
     _, _, g_siegman   = tr.naive_siegman(fpy[:n_samples], dln, r0, k0)
+    
+    equivalent_fresnel = r0 # optimal fresnel
+    _, _, g_magni = tr.fhta_single(fpy[:n_samples], dln, r0, fresnel=equivalent_fresnel) # r0 is optimized with Eq.(5)
     
     plt.figure(figsize=(4, 2.5))
     plt.plot(idxp, fpy, label='Original Data')
@@ -89,7 +116,7 @@ def test_siegman(function=flat_top, label="ft", fresnel=1):
     # physics
     exact = function(log_k_p, transform=True)
     plt.figure(figsize=(4, 2.5))
-    plt.plot(log_k_p, g / (log_k_p * fresnel), label='Transformed Data')
+    plt.plot(log_k_p, g_scipy / (log_k_p * fresnel), label='Transformed Data')
     plt.plot(log_k_p, exact, label='Transformed Data')
     plt.xlabel(r'$k$')
     plt.xlim(0, n_samples/4)
@@ -100,14 +127,15 @@ def test_siegman(function=flat_top, label="ft", fresnel=1):
     print("Saved to ", name)
     
     # samples
-    exact = function(log_k_p, transform=True) * (log_k_p * fresnel)
+    exact = function(log_k_p, transform=True)
     plt.figure(figsize=(4, 2.5))
-    plt.plot(idxp, g, label="scipy")
-    plt.plot(idxp, g_siegman, label="siegman")
-    plt.plot(idxp, exact, label="exact")
+    plt.plot(idxp, g_scipy, label="scipy", lw=0.1)
+    plt.plot(idxp, g_siegman, label="siegman", lw=0.3, ls="--")
+    plt.plot(idxp, g_magni * log_k_p, label="magni", lw=0.1)
+    plt.plot(idxp, exact/(np.pi), label="exact", lw=0.1)
     plt.legend()
     plt.xlabel('idx')
-    plt.axvline(n_samples, color='red', linestyle='--', label='n_samples')
+    plt.axvline(n_samples, color='red', linestyle='--', label='n_samples', lw = 0.1)
     # plt.axvline(np.log(1/log_k_p[-1]) * n_samples, color='red', linestyle='--', label='n_samples')
     # plt.xlim(0, 100)
     plt.tight_layout()
@@ -154,52 +182,48 @@ def test_naive_siegman():
     plt.savefig('media/naive.pdf')
 
 
-def test_naive_magni(fresnel = 1, clip_reference = 1):
-    n_samples = 400
+def test_naive_magni(fresnel = 10, clip_reference = 1):
+    n_samples = 128 # with 128 it is really really good
+    # FIXME hypotesis: one needs to be aware of how high it sets the minimum radius. it is not good to have it too low, since then an enormous number of radii will be low.
     idx = np.arange((n_samples))
     idxp = np.arange((2*n_samples))
-    r0  = 0.006349
-    dln = 0.01612
+   
+    log_r_magni, r0, dln = tr.magni_grid(n_samples)
+    log_r_p = r0 * np.exp((idxp - n_samples + 1) * dln) # this is the first value of the log_r, by construction
+    print("selected range:", log_r_magni[:4], "...", log_r_magni[-4:])
+    print("first value: ", r0,"last value:", log_r_magni[-1])
+    log_k     = log_r_magni
+    log_k_p   = log_r_p
     
-    # desired_r_max = 1.0
-    # dln = np.log(desired_r_max / r0) / (n_samples - 1)
-    
-    print("dln:", dln)
-    log_r   = r0 * np.exp(idx * dln)
-    log_r_magni = tr.magni_grid(n_samples, dln, r0)
-    
-    print("selected range:", log_r[:4], "...", log_r[-4:])
-    print("last value:", log_r[-1])
-    # assert np.isclose(log_r[-1], 3.938)
-    log_k   = 1 / r0 * np.exp((idx - n_samples + 1) * dln)
-    log_k_p   = 1 / r0 * np.exp((idxp - n_samples + 1) * dln)
-    
-    f       = magni(log_r, transform=False)
+    f       = magni(log_r_magni, transform=False)
     f_magni = magni(log_r_magni, transform=False)
-    fs = log_r * f
+    fs   = log_r_magni * f
     fs_m = log_r_magni * f_magni
+    fs_m = f_magni
     # fs = np.pad(fs, (0, n_samples), 'constant', constant_values=0) -> we are already padding  
-    equivalent_k0 = r0 * fresnel / (2*np.pi) # for the classical Siegman method
-    _, _, g   = tr.naive_siegman(fs, dln, r0, equivalent_k0)
-    _, _, g_m = tr.fhta_single(fs_m, dln, r0, fresnel=fresnel)
-    g_m /= 2 * np.pi #  FIXME this trial is probably wrong
-    g /= log_k_p # this seems to be needed. Still the oscillations seem to be wrong. 
+    equivalent_k0 = r0 * fresnel # for the classical Siegman method
+    _, _, g_siegman   = tr.naive_siegman(fs, dln, r0, equivalent_k0)
+    _, _, g_m = tr.fhta_single(fs_m, fresnel=fresnel)
+    # _, _, g_m_fool = tr.foolproof_magni(magni, n_samples, fresnel)
+    g_siegman /= log_k_p # this seems to be needed. Still the oscillations seem to be wrong.
     
     # We notice (check.pdf)
-    # - the magni transform is out of scale, but behaves good (about 11 over 1.3 desired)
+    # - the magni transform is out of scale, but behaves good (fabout 11 over 1.3 desired)
     # - the Siegman transform is very much in scale, but the equivalent k0 I think we are missing it
     # - every transform starts from the same point at null k 
     # assert np.isclose(g, 0.0)
     
-    exact = magni(2* np.pi * fresnel * log_r_magni, transform=True) # adapt for the calculation with the actual eta
-    siegman = g
+    exact = magni(2 * np.pi * fresnel * log_k * (1-1/n_samples), transform=True) # adapt for the calculation with the actual eta
     fig, ax1 = plt.subplots(figsize=(5, 4))
     color1 = 'tab:blue'
-    color1b = 'orange'
-    for reference in [exact, siegman][:clip_reference]:
-        ax1.plot(log_r_magni, np.real(reference)[:n_samples], lw=1.2, ls="-")
+    color2 = 'green'
+    color1b = 'black'
+    for reference in [exact, g_siegman][:clip_reference]:
+        ax1.plot(log_k, np.real(reference)[:n_samples], lw=0.1, ls="-")
         # ax1.plot(log_r_magni, np.imag(reference)[:n_samples], lw=1.2, ls=":")
    
+    ax1.plot(log_k, np.real(g_m)[:n_samples], lw=0.1, ls="-", color="red")
+    # ax1.plot(log_k, np.real(g_m_fool)[:n_samples], lw=0.5, ls=":", color=color1b)
     ax1.set_ylabel("reference", color=color1)
     ax1.tick_params(axis='y', labelcolor=color1)
     # Shared x-axis
@@ -209,23 +233,36 @@ def test_naive_magni(fresnel = 1, clip_reference = 1):
     # T_max_high = 
     # Second axis: T_max
     ax2 = ax1.twinx()
-    color2 = 'tab:red'
 
-    ax2.plot(log_r_magni, np.real(g_m)[:n_samples], lw=1.2, ls="-", color=color2)
-    ax2.set_ylabel("magni", color=color2)
+    # ax2.plot(log_k, np.real(g_siegman)[:n_samples], lw=0.1, ls="-", color=color2)
+    # ax2.set_ylabel("siegman", color=color2)
     ax2.tick_params(axis='y', labelcolor=color2)
     # ax2.plot(log_r_magni, np.imag(g_m)[:n_samples], lw=1.2, ls=":")
     ax2.ticklabel_format(style='sci', axis='y', scilimits=(3, 0))
-    plt.xlabel('idx')
+    ax1.axhline(0, color='black', lw=0.1, ls="--")
     plt.tight_layout()
     name = 'media/naive.pdf'
     plt.savefig(name)
     print(f"Saved plot to {name}")
   
+    plt.figure(figsize=(6, 3.5))
+    plt.plot(log_k, np.clip(np.real(g_m[:n_samples]-exact), a_min =-1e-2, a_max=1e-2  ),   lw=0.6, ls="-", label=f"abs error,max of exact = {np.max(np.abs(exact)):.2f}")
+    # plt.plot(log_k, np.clip(np.real(g_m_fool[:n_samples]-exact), a_min =-1e-2, a_max=1e-2  ),   lw=0.6, ls="-", label=f"abs error foolproof")
+    plt.plot(log_k, np.clip(np.real(g_siegman[:n_samples]/g_siegman[0]*exact[0]-exact), a_min =-1e-2, a_max=1e-2),   lw=0.6, ls="-", label=f"abs err siegman (cheating)")
+    plt.xlabel(r'$k$')
+    plt.ylim([-2e-3, 2e-3])
+    plt.legend()
+    plt.tight_layout()
+    name = 'media/error.pdf'
+    plt.savefig(name)
+    print(f"Saved plot to {name}")
     
 if __name__ == "__main__":
-    test_siegman(function=flat_top, label="ft", fresnel = 1)
+    test_naive_magni(clip_reference=1)
     exit()
+    for ff in [flat_top, magni]:
+        test_siegman(function=ff, label=ff.__name__, fresnel=1)
+    # test_siegman(function=magni, label="ft", fresnel = 10)
     test_naive_magni(clip_reference=2)
     test_siegman(function=flat_top, label="ft", fresnel = 1)
     test_siegman(function=magni,    label="mg", fresnel = 3)
